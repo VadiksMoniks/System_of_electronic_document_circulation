b6ad423a338bb52a6f8906ec61f328d456dad411
VadiksMoniks <andr26012k191@gmail.com>
2022-11-12 23:57:53 +0200

    fixed bag with including views

--- views/template_view.php
+++ views/template_view.php
@@ -129,8 +129,18 @@
         </div>
         <div id="content">
         <?php
-        
-            include 'E:/xampp/htdocs/System_of_electronic_document_circulation/views/'.basename($_SERVER['PATH_INFO']).'_view.php';
+            
+            if(!isset($_SERVER['PATH_INFO'])){
+                include 'E:/xampp/htdocs/System_of_electronic_document_circulation/views/main_view.php';
+            }
+            else{
+                if(file_exists('E:/xampp/htdocs/System_of_electronic_document_circulation/views/'.basename($_SERVER['PATH_INFO']).'_view.php')){
+                    include 'E:/xampp/htdocs/System_of_electronic_document_circulation/views/'.basename($_SERVER['PATH_INFO']).'_view.php';
+                }
+                else{
+                    echo '404';
+                }
+            }
         ?>
         </div>
         <div id="footer"></div>
